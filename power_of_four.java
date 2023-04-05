b23a59cc85580f40a424504be790963f1ace1dac
Roman34Sh <93519715+Roman34Sh@users.noreply.github.com>
2023-04-06 02:29:32 +0300

    Create power_of_four.java

---
+++ power_of_four.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n == 1){
+           return true;     
+        } 
+          if (n < 1){
+           return false;     
+        }
+        
+  
+    while (n%4==0)
+    {
+      n=n/4; 
+    }
+        if (n==1){
+            return true;
+        }
+       else 
+          return false;
+    }
+}
